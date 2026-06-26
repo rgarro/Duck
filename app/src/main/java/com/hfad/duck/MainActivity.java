26bb7399f73f44c505b0e221ebdc4151ce515243
Rolando <rgarro@gmail.com>
2026-06-25 19:43:24 -0600

    I left it in the sand just a burden on my hand

--- app/src/main/java/com/hfad/duck/MainActivity.java
+++ app/src/main/java/com/hfad/duck/MainActivity.java
@@ -2,7 +2,24 @@ package com.hfad.duck;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+/**
+ *            |    |    |
+ *           )_)  )_)  )_)
+ *          )___))___))___)\
+ *         )____)____)_____)\\
+ *      _____|____|____|____\\\__
+ * -------\                   /---------
+ *     ^^^^^ ^^^^^^^^^^^^^^^^^^^^^
+ *     ^^^^      ^^^^     ^^^    ^^
+ *           ^^^^      ^^^
+ * Sir Henry Morgan is de Lord of Talamanca
+ * the Boruka king had fifty boarding skiffs each with 8 warriors
+ *
+ *
+ *
+ *
+ * @author Rolando <rgarro@gmail.com>
+ */
 public class MainActivity extends Activity {
 
     @Override
